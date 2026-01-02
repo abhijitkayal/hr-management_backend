b546e0803ca722d525210909dc7946722a942b7d
abhijitkayal <153314653+abhijitkayal@users.noreply.github.com>
2026-01-02 21:14:29 +0530

    Update feedbackRoutesforclient.js

--- server/routes/feedbackRoutesforclient.js
+++ server/routes/feedbackRoutesforclient.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Feedback = require("../models/feedback");
+const Feedback = require("../models/Feedback");
 
 // ✅ GET ALL FEEDBACK (ADMIN)
 router.get("/", async (req, res) => {
