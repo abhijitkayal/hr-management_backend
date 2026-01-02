7468d619e2846892760c2c2d343d61d8d91be4a7
abhijitkayal <153314653+abhijitkayal@users.noreply.github.com>
2026-01-02 21:04:31 +0530

    Update clientRoutes.js

--- server/routes/clientRoutes.js
+++ server/routes/clientRoutes.js
@@ -24,7 +24,7 @@
 
 const express = require("express");
 const router = express.Router();
-const Client = require("../models/client");
+const Client = require("../models/Client");
 
 /* ===================== CREATE CLIENT ===================== */
 router.post("/", async (req, res) => {
